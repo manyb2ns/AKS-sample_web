de8c73118c6574b025f4fdca38aca5579011af93
root <root@jdb-vm-01.qzh2yztthtoetnhlodzbpc05ie.syx.internal.cloudapp.net>
2024-11-11 05:27:21 +0000

    init commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# 베이스 이미지로 Python 사용
+FROM python:3.9-slim
+
+# 앱 디렉터리 생성
+WORKDIR /app
+
+# 필요한 파일 복사
+COPY app.py /app
+COPY templates /app/templates
+COPY static /app/static
+
+# Flask 설치
+RUN pip install Flask
+
+# 앱 실행
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    #return "<h1>Welcome to the Home Page</h1><p>This is the main page of our application.</p>"
+    return render_template('home.html')
+
+@app.route('/page1')
+def page1():
+    #return "<h1>Page 1</h1><p>This is the content for Page 1.</p>"
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    #return "<h1>Page 2</h1><p>This is the content for Page 2.</p>"
+    return render_template('page2.html')
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
