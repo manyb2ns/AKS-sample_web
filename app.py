fc101164978372b1960ae6227b37871eb4ae7fed
manyb2ns <manyb2ns@gmail.com>
2024-11-28 04:58:31 +0000

    Dockerfile 생성

--- app.py
+++ app.py
@@ -18,5 +18,5 @@ def page2():
     return render_template('page2.html')
 
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=80)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True)
